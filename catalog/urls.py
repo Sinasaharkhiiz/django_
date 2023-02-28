dc93f03009364c16b0990e2df55aad8473d19ff0
Sinasaharkhiiz <Sinasaharkhiiz@gmail.com>
2023-02-28 17:47:47 +0330

    V2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import include, path
+from . import views
+app_name = 'catalog'
 urlpatterns = [
-  
+  path('', views.index.as_view() , name='index'),
+  path('books/', views.BookListView.as_view() , name='books'),
+  path('book/(?P<pk>\d+)',  views.BookDetailView.as_view(), name='book_detail'),
+  path('authors/',  views.AuthorListView.as_view(), name='authors')
   ]
  
